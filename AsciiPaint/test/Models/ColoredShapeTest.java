6c51eef9ce8855c41edfad85cdebff38e3077c61
Léopold Mols <53212@etu.he2b.be>
2019-10-16 14:40:40 +0200

    Updated tests

--- AsciiPaint/test/Models/ColoredShapeTest.java
+++ AsciiPaint/test/Models/ColoredShapeTest.java
@@ -74,5 +74,4 @@ public class ColoredShapeTest {
             throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
     }
-    
 }
