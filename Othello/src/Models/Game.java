7238817c2601cdeb7291f8a7c2118e78c0a55f6c
Léopold Mols <53212@etu.he2b.be>
2019-11-03 21:31:18 +0100

    Othello not finished yet

--- Othello/src/Models/Game.java
+++ Othello/src/Models/Game.java
@@ -1,10 +1,12 @@
 package Models;
 
+import java.util.List;
+
 /**
  *
  * @author leopoldmols
  */
-public class Game {
+public class Game implements Model{
     
     private Board board;
     private Player current;
@@ -17,6 +19,7 @@ public class Game {
         oponent = new Player(Color.BLACK);
     }
     
+    @Override
     public void initialize()
     {
         this.board = new Board ();
@@ -32,6 +35,58 @@ public class Game {
         Position posp4 = new Position(6, 6);
         
         
+    }
+    
+    /**
+     * This methods overrides the methods "start()" received from the class Model and verifies if the playing board exists and
+     * if any piece is over any other piece via the following method in this class
+     *
+     * This method throws a IllegalStateException("Le plateau de jeu n'a pas été initialisé !") if the board doesn't exist
+     * This method throws a IllegalArgumentException("La partie est terminée !") if the game is finished
+     */
+    @Override
+    public void start() {
+        if (board == null) {
+            throw new IllegalStateException("Le plateau de jeu n'a pas été initialisé !");
+        }
+        if (isOver() == true) {
+            throw new IllegalArgumentException("La partie est terminée !");
+        }
+    }
+
+    @Override
+    public boolean isOver() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Square[][] getBoard() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void select(int row, int column) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Piece getSelected() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Player getCurrent() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Player> getWinners() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Player> getLoosers() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
---
+++ Othello/src/Models/Model.java
@@ -0,0 +1,74 @@
+package Models;
+
+import java.util.List;
+
+/**
+ *
+ * @author leopoldmols
+ */
+public interface Model {
+    
+    /**
+     * Initializes a new game with a default board
+     */
+    void initialize();
+    
+    /**
+     * Checks if a match can start
+     * 
+     * @throws IllegalStateException if no board is set.
+     * @throws IllegalStateException if the board set is incomplete.
+     * 
+     */
+    void start();
+    
+    /**
+     * Declares if it's the end of the game.
+     *
+     * @return true if it's the end of the game.
+     */
+    boolean isOver();
+    
+    /**
+     * Gets the playing board
+     * 
+     * @return the board.
+     */
+    Square [][] getBoard();
+    
+    /**
+     * Select a position at a position to make it change of color
+     * 
+     * @param row the row of the position
+     * @param column the column of the position
+     */
+    void select (int row, int column);
+    
+    /**
+     * Gets the piece at a selected position
+     * 
+     * @return the position of a selected piece
+     */
+    Piece getSelected();
+    
+    /**
+     * This method gives the current player
+     * 
+     * @return the player that has to play now
+     */
+    Player getCurrent();
+    
+    /**
+     * Gets the list of winners (even if there will only be one winner)
+     * 
+     * @return the list of the winners of a hole game
+     */
+    List<Player> getWinners();
+    
+    /**
+     * Gets the list of loosers (even if there will only be one looser)
+     * 
+     * @return the list of the loosers of a hole game
+     */
+    List<Player> getLoosers();
+}
