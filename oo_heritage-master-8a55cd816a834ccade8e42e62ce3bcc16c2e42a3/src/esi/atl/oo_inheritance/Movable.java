adea747d7319ffab88bc8b064deda62df480c6af
Léopold Mols <53212@etu.he2b.be>
2019-11-13 11:27:24 +0100

    backup1

---
+++ oo_heritage-master-8a55cd816a834ccade8e42e62ce3bcc16c2e42a3/src/esi/atl/oo_inheritance/Movable.java
@@ -0,0 +1,5 @@
+package esi.atl.oo_inheritance;
+
+public interface Movable {
+    public Movable move(double dx, double dy);
+}
---
+++ oo_heritage-master-8a55cd816a834ccade8e42e62ce3bcc16c2e42a3/src/esi/atl/oo_inheritance/PinnablePoint.java
@@ -0,0 +1,44 @@
+package esi.atl.oo_inheritance;
+
+public class PinnablePoint extends Point {
+    
+    private boolean pinned; // once pinned cannot move.
+    
+    public PinnablePoint(double x, double y) {
+        super(x, y);
+        this.pinned = false;
+    }
+    
+    public void pin() { pinned = true; } // once pinned, no way to unpin.
+    
+    @Override
+    public Point move(double dx, double dy) {
+        if(!pinned)
+            super.move(dx, dy);
+        // do nothing if pinned.
+        return this;
+    }
+    
+//    @Override
+//    public Point move(double dx, double dy) throws Exception {
+//        if (! pinned) super.move(dx, dy);
+//        else throw new Exception("Point is pinned, cannot move anymore");
+//        return this;
+//    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + " - "+ (pinned? "pinned": "not pinned");
+    }
+    
+    public static void main(String[] args) {
+        Point p = new PinnablePoint(0, 0);
+        System.out.println(p);
+        p.move(1, 1);
+        PinnablePoint pp = (PinnablePoint)p; // we know it is a PinnablePoint
+        pp.pin();
+        p.move(1,1);
+        System.out.println(p);
+    }
+}
+
